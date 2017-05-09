386d108a110e75f479c6a40711c26e37464e8bf6
Raphael Voellmy <r.voellmy@gmail.com>
2017-05-09 13:14:20 +0200

    Set up gulp dist task Add babelrc config

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,50 @@
 const gulp = require('gulp');
 const plugins = require('gulp-load-plugins')();
 const babel = require('rollup-plugin-babel');
+const runSequence = require('run-sequence');
 
 gulp.task('build', () => gulp
-    .src('src/index.js')
-    .pipe(plugins.sourcemaps.init())
-    .pipe(plugins.betterRollup({
-      external: ['angular'],
-      // notice there is no `entry` option as rollup integrates into gulp pipeline
-      plugins: [babel({
-        // presets: ['es2015'],
-        exclude: 'node_modules/**',
-        plugins: ['transform-function-bind'],
-      })],
-    }, {
-      // also rollups `sourceMap` option is replaced by gulp-sourcemaps plugin
-      format: 'cjs',
-    }))
-    // inlining the sourcemap into the exported .js file
-    .pipe(plugins.sourcemaps.write())
-    .pipe(gulp.dest('dist'))
-);
+  .src('src/index.js')
+  .pipe(plugins.plumber())
+  .pipe(plugins.sourcemaps.init())
+  .pipe(plugins.sourcemaps.identityMap())
+  .pipe(plugins.betterRollup({
+    external: ['angular'],
+    globals: ['angular'],
+    plugins: [
+      babel({ exclude: 'node_modules/**' }),
+    ],
+  }, {
+    moduleName: 'ngEasyDropdown',
+    format: 'umd',
+    globals: {
+      angular: 'angular',
+    },
+  }))
+  .pipe(plugins.sourcemaps.write())
+  .pipe(plugins.rename('ng-easy-dropdown.js'))
+  .pipe(gulp.dest('dist')));
 
+gulp.task('minify', () => gulp
+  .src(['dist/*.js', '!dist/*.min.js'])
+  .pipe(plugins.sourcemaps.init({ loadMaps: true }))
+  .pipe(plugins.sourcemaps.identityMap())
+  .pipe(plugins.uglify({
+    source_map: true,
+  }))
+  .pipe(plugins.rename((path) => {
+    // eslint-disable-next-line no-param-reassign
+    path.extname = `.min${path.extname}`;
+  }))
+  .pipe(plugins.sourcemaps.write('.'))
+  .pipe(gulp.dest('dist')));
 
-// gulp.task('build', () => gulp
-//   .src('src/*.js')
-//   .pipe(plugins.sourcemaps.init())
-//   .pipe(plugins.babel({
-//     presets: ['es2015'],
-//     plugins: ['transform-function-bind'],
-//   }))
-//   .pipe(plugins.sourcemaps.write('.'))
-//   .pipe(gulp.dest('build')));
-//
-// gulp.task('browserify', () => gulp
-//   .src('build/index.js')
-//   .pipe(plugins.browserify({
-//     insertGlobals: false,
-//     external: ['angular'],
-//   }))
-//   .pipe(gulp.dest('dist')));
-//
+gulp.task('dist', cb => runSequence('clean', 'build', 'minify', cb));
+
+gulp.task('clean', () => gulp
+  .src('dist')
+  .pipe(plugins.clean()));
+
 gulp.task('watch', () => gulp.watch('src/*.js', ['build']));
-//
-// gulp.task('default', ['build', 'browserify']);
+
+gulp.task('default', ['build', 'watch']);
